7c660f947bd8377943807d59cd76d3e174f5d212
whuwxl <whuwxl@gmail.com>
2023-04-24 16:54:58 +0800

    add test

--- main.go
+++ main.go
@@ -6,12 +6,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
+func setupRouter() *gin.Engine {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
+
+	return r
+}
+
+func main() {
+	r := setupRouter()
 	r.Run()
 }
